0756f157c81f67d9f733a5a5e2f3b59ec0c75e5f
Spencer Curtis <spencercolecurtis@gmail.com>
2016-03-23 13:44:24 -0600

    Working on custom colors for views

---
+++ CapstoneReminder/AppColorPalette.swift
@@ -0,0 +1,17 @@
+//
+//  AppColorPalette.swift
+//  CapstoneReminder
+//
+//  Created by Spencer Curtis on 3/23/16.
+//  Copyright © 2016 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor {
+    
+    static func customCyanColor()  -> UIColor {
+        return UIColor(red: 79/255, green: 204/255, blue: 247/255, alpha: 1.0)
+    }
+    
+}
---
+++ CapstoneReminder/AppearanceController.swift
@@ -0,0 +1,22 @@
+//
+//  AppearanceController.swift
+//  CapstoneReminder
+//
+//  Created by Spencer Curtis on 3/23/16.
+//  Copyright © 2016 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppearanceController {
+    
+    static func initializeAppearanceDefaults() {
+        UISegmentedControl.appearance().tintColor = customcyan
+        
+        
+    }
+    
+    
+}
+
