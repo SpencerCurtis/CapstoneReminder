93ecd96fb86b727c5a89b46b45905b659b587e6a
Spencer Curtis <spencercolecurtis@gmail.com>
2016-05-10 13:19:38 -0600

    Final changes before submitting (hopefully) the last TestFlight before submitting to the App Store.

--- CapstoneReminder/AppDelegate.swift
+++ CapstoneReminder/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         AppearanceController.initializeAppearanceDefaults()
-        
+        NSThread.sleepForTimeInterval(1.0)
         if NSUserDefaults.standardUserDefaults().boolForKey("firstRun") == false {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let pageViewController = storyboard.instantiateViewControllerWithIdentifier("PageViewController")
